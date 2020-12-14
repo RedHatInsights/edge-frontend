0f76facde101e304c0d1b829bda638d3f68e9487
Karel Hala <khala@redhat.com>
2020-12-14 15:23:29 +0100

    Update to webpack 5

--- profiles/local-frontend-and-api.js
+++ profiles/local-frontend-and-api.js
@@ -6,10 +6,14 @@ const FRONTEND_PORT = 8002;
 const API_PORT = 8888;
 const routes = {};
 
-routes[`/beta/${SECTION}/${APP_ID}`] = { host: `https://localhost:${FRONTEND_PORT}` };
-routes[`/${SECTION}/${APP_ID}`]      = { host: `https://localhost:${FRONTEND_PORT}` };
-routes[`/beta/apps/${APP_ID}`]       = { host: `https://localhost:${FRONTEND_PORT}` };
-routes[`/apps/${APP_ID}`]            = { host: `https://localhost:${FRONTEND_PORT}` };
+routes[`/beta/${SECTION}/${APP_ID}`] = {
+  host: `https://localhost:${FRONTEND_PORT}`,
+};
+routes[`/${SECTION}/${APP_ID}`] = {
+  host: `https://localhost:${FRONTEND_PORT}`,
+};
+routes[`/beta/apps/${APP_ID}`] = { host: `https://localhost:${FRONTEND_PORT}` };
+routes[`/apps/${APP_ID}`] = { host: `https://localhost:${FRONTEND_PORT}` };
 
 routes[`/api/${APP_ID}`] = { host: `https://localhost:${API_PORT}` };
 
--- profiles/local-frontend.js
+++ profiles/local-frontend.js
@@ -5,9 +5,13 @@ const APP_ID = 'edge';
 const FRONTEND_PORT = 8002;
 const routes = {};
 
-routes[`/beta/${SECTION ? `/${section}` : ''}${APP_ID}`] = { host: `https://localhost:${FRONTEND_PORT}` };
-routes[`/${SECTION ? `/${section}` : ''}${APP_ID}`]      = { host: `https://localhost:${FRONTEND_PORT}` };
-routes[`/beta/apps/${APP_ID}`]       = { host: `https://localhost:${FRONTEND_PORT}` };
-routes[`/apps/${APP_ID}`]            = { host: `https://localhost:${FRONTEND_PORT}` };
+routes[`/beta/${SECTION ? `/${section}` : ''}${APP_ID}`] = {
+  host: `https://localhost:${FRONTEND_PORT}`,
+};
+routes[`/${SECTION ? `/${section}` : ''}${APP_ID}`] = {
+  host: `https://localhost:${FRONTEND_PORT}`,
+};
+routes[`/beta/apps/${APP_ID}`] = { host: `https://localhost:${FRONTEND_PORT}` };
+routes[`/apps/${APP_ID}`] = { host: `https://localhost:${FRONTEND_PORT}` };
 
 module.exports = { routes };
