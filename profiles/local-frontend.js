cd2384b651667a05dbd89a925e73aa683cbb84a7
Karel Hala <khala@redhat.com>
2020-12-14 14:39:42 +0100

    Add basic routing

--- profiles/local-frontend.js
+++ profiles/local-frontend.js
@@ -1,12 +1,12 @@
 /*global module*/
 
-const SECTION = 'insights';
-const APP_ID = 'starter';
+const SECTION = '';
+const APP_ID = 'edge';
 const FRONTEND_PORT = 8002;
 const routes = {};
 
-routes[`/beta/${SECTION}/${APP_ID}`] = { host: `https://localhost:${FRONTEND_PORT}` };
-routes[`/${SECTION}/${APP_ID}`]      = { host: `https://localhost:${FRONTEND_PORT}` };
+routes[`/beta/${SECTION ? `/${section}` : ''}${APP_ID}`] = { host: `https://localhost:${FRONTEND_PORT}` };
+routes[`/${SECTION ? `/${section}` : ''}${APP_ID}`]      = { host: `https://localhost:${FRONTEND_PORT}` };
 routes[`/beta/apps/${APP_ID}`]       = { host: `https://localhost:${FRONTEND_PORT}` };
 routes[`/apps/${APP_ID}`]            = { host: `https://localhost:${FRONTEND_PORT}` };
 
---
+++ src/Routes/Groups/Groups.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Groups = () => {
+    return <div>Here will be groups!</div>;
+};
+
+export default Groups;
