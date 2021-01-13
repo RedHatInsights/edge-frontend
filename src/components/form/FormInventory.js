5315d89fcbff31b88335e25f5d4f6091e97aae97
Karel Hala <khala@redhat.com>
2021-01-13 16:35:37 +0100

    Add proper bulk select functionality (#10)

--- src/components/form/FormInventory.js
+++ src/components/form/FormInventory.js
@@ -2,8 +2,14 @@ import React, { Fragment, useRef } from 'react';
 import { InventoryTable } from '@redhat-cloud-services/frontend-components/components/esm/Inventory';
 import PropTypes from 'prop-types';
 import { InventoryContext } from './constants';
-import { Provider } from 'react-redux';
+import { Provider, useSelector, useDispatch } from 'react-redux';
 import { entitiesReducer } from '../../store/inventory-table';
+import { selectEntity } from '../../store/actions';
+
+const calculateChecked = (rows = [], selected) =>
+  rows.every(({ id }) => selected?.has(id))
+    ? rows.length > 0
+    : rows.some(({ id }) => selected?.has(id)) && null;
 
 const FormInventoryTable = ({
   name,
@@ -12,9 +18,16 @@ const FormInventoryTable = ({
   FormSpyProvider,
   formOptions,
   component,
+  registry,
   ...rest
 }) => {
+  const dispatch = useDispatch();
   const inventory = useRef(null);
+  const selected = useSelector(
+    ({ entities }) => entities?.selected || new Map()
+  );
+  const loaded = useSelector(({ entities }) => entities?.loaded);
+  const rows = useSelector(({ entities }) => entities?.rows);
   const onRefresh = (options, callback) => {
     if (!callback && inventory && inventory.current) {
       inventory.current.onRefreshData(options);
@@ -22,33 +35,63 @@ const FormInventoryTable = ({
       callback(options);
     }
   };
+  const calculateSelected = () => (selected ? selected.size : 0);
 
   return (
-    <InventoryContext.Consumer>
-      {({ registry }) => {
-        return registry ? (
-          <Provider store={registry.getStore()}>
-            <InventoryTable
-              {...rest}
-              ref={inventory}
-              onRefresh={onRefresh}
-              tableProps={{
-                canSelectAll: false,
-              }}
-              onLoad={({ mergeWithEntities }) => {
-                registry?.register?.(mergeWithEntities(entitiesReducer()));
-              }}
-            />
-          </Provider>
-        ) : (
-          <Fragment />
-        );
+    <InventoryTable
+      {...rest}
+      bulkSelect={{
+        count: calculateSelected(),
+        items: [
+          {
+            title: 'Select none (0)',
+            onClick: () => {
+              dispatch(selectEntity(-1, false));
+            },
+          },
+          {
+            ...(loaded && rows && rows.length > 0
+              ? {
+                  title: `Select page (${rows.length})`,
+                  onClick: () => {
+                    dispatch(selectEntity(0, true));
+                  },
+                }
+              : {}),
+          },
+        ],
+        checked: calculateChecked(rows, selected),
+        onSelect: (value) => {
+          dispatch(selectEntity(0, value));
+        },
       }}
-    </InventoryContext.Consumer>
+      ref={inventory}
+      onRefresh={onRefresh}
+      tableProps={{
+        canSelectAll: false,
+      }}
+      onLoad={({ mergeWithEntities }) => {
+        registry?.register?.(mergeWithEntities(entitiesReducer()));
+      }}
+    />
   );
 };
 
-FormInventoryTable.propTypes = {
+const WrappedFormInventoryTable = (props) => (
+  <InventoryContext.Consumer>
+    {({ registry }) =>
+      registry ? (
+        <Provider store={registry.store}>
+          <FormInventoryTable registry={registry} {...props} />
+        </Provider>
+      ) : (
+        <Fragment />
+      )
+    }
+  </InventoryContext.Consumer>
+);
+
+WrappedFormInventoryTable.propTypes = {
   FieldProvider: PropTypes.any,
   formOptions: PropTypes.any,
   FormSpyProvider: PropTypes.any,
@@ -58,4 +101,11 @@ FormInventoryTable.propTypes = {
   component: PropTypes.any,
 };
 
-export default FormInventoryTable;
+FormInventoryTable.propTypes = {
+  ...WrappedFormInventoryTable.propTypes,
+  registry: PropTypes.shape({
+    register: PropTypes.func,
+  }),
+};
+
+export default WrappedFormInventoryTable;
