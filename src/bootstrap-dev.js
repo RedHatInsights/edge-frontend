81468d0a4c30f66b062dbb4e29b49a43936aac9e
Karel Hala <khala@redhat.com>
2021-02-11 08:53:21 +0100

    Loging props

--- src/bootstrap-dev.js
+++ src/bootstrap-dev.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import InventoryApp from './AppEntry';
 import logger from 'redux-logger';
 
+console.log(document.getElementById('root'), 'This is root element');
+console.log(insights?.chrome, 'this is chrome!');
+
 ReactDOM.render(
   <InventoryApp logger={logger} />,
   document.getElementById('root')
