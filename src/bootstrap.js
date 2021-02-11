1f490fc8f2156e92581ad36e5deb2c677adc78ce
Karel Hala <khala@redhat.com>
2021-02-11 09:07:04 +0100

    Add logging to stable

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -2,4 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import InventoryApp from './AppEntry';
 
+console.log(document.getElementById('root'), 'This is root element');
+console.log(insights?.chrome, 'this is chrome!');
+
 ReactDOM.render(<InventoryApp />, document.getElementById('root'));
