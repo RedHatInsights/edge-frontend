79378d70c83267e4db922ee26b7420bb84ea3995
Karel Hala <khala@redhat.com>
2021-03-19 16:10:04 +0100

    Add custom edge system profile fields

--- src/components/DeviceDetail.js
+++ src/components/DeviceDetail.js
@@ -1,5 +1,7 @@
 import React, { Suspense, lazy } from 'react';
 import { useStore, useSelector } from 'react-redux';
+import { OutlinedQuestionCircleIcon } from '@patternfly/react-icons';
+import { Tooltip } from '@patternfly/react-core';
 
 const GeneralInformation = lazy(() =>
   import(
@@ -18,10 +20,38 @@ const OperatingSystemCard = lazy(() =>
   )
 );
 
+const CollectionCard = lazy(() =>
+  import(
+    '@redhat-cloud-services/frontend-components-inventory-general-info/CollectionCard'
+  )
+);
+
+import {
+  generalMapper,
+  statusHelper,
+} from '@redhat-cloud-services/frontend-components-inventory-general-info/dataMapper';
+
 const GeneralInformationTab = () => {
   const writePermissions = useSelector(
     ({ permissionsReducer }) => permissionsReducer?.writePermissions
   );
+
+  const {
+    runningVersion,
+    stagedVersion,
+    nonActiveVersion,
+    heathCheck,
+    rhcHealth,
+  } = useSelector(({ systemProfileStore }) => ({
+    runningVersion:
+      systemProfileStore?.systemProfile?.running_rpm_os_tree_version,
+    stagedVersion:
+      systemProfileStore?.systemProfile?.staged_rpm_os_tree_version,
+    nonActiveVersion:
+      systemProfileStore?.systemProfile?.non_active_rpm_os_tree_version || [],
+    heathCheck: systemProfileStore?.systemProfile?.health_check,
+    rhcHealth: systemProfileStore?.systemProfile?.rhc_health,
+  }));
 
   return (
     <Suspense fallback="">
@@ -36,7 +66,54 @@ const GeneralInformationTab = () => {
         )}
         OperatingSystemCardWrapper={(props) => (
           <Suspense fallback="">
-            <OperatingSystemCard {...props} hasKernelModules={false} />
+            <OperatingSystemCard
+              {...props}
+              hasKernelModules={false}
+              extra={[
+                { title: 'Running rpm-ostree version', value: runningVersion },
+                {
+                  title: 'Staged rpm-ostree version',
+                  value: stagedVersion,
+                },
+                {
+                  title: 'Non-active (available rollback version(s))',
+                  value: nonActiveVersion?.length,
+                  plural: 'versions',
+                  singular: 'version',
+                  onClick: (_e, handleClick) =>
+                    handleClick(
+                      'Non-active (available rollback version(s))',
+                      generalMapper(nonActiveVersion || [], 'Version'),
+                      'small'
+                    ),
+                },
+                {
+                  title: 'Health check status',
+                  value: statusHelper[heathCheck?.toUpperCase()] || (
+                    <Tooltip content="Unknown service status">
+                      <OutlinedQuestionCircleIcon className="ins-c-inventory__detail--unknown" />
+                    </Tooltip>
+                  ),
+                },
+              ]}
+            />
+          </Suspense>
+        )}
+        CollectionCardWrapper={(props) => (
+          <Suspense fallback="">
+            <CollectionCard
+              {...props}
+              extra={[
+                {
+                  title: 'RHC Health (broker functioning)',
+                  value: statusHelper[rhcHealth?.toUpperCase()] || (
+                    <Tooltip content="Unknown service status">
+                      <OutlinedQuestionCircleIcon className="ins-c-inventory__detail--unknown" />
+                    </Tooltip>
+                  ),
+                },
+              ]}
+            />
           </Suspense>
         )}
       />
