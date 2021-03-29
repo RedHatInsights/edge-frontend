d867c78c56f1fbb6aabb8ed3d5e3230351c99798
Karel Hala <khala@redhat.com>
2021-03-29 20:44:05 +0200

    Remove unused routes (#22)

--- src/Routes.js
+++ src/Routes.js
@@ -3,14 +3,14 @@ import { Redirect, Route, Switch } from 'react-router-dom';
 import React, { Suspense } from 'react';
 import { routes as paths } from '../package.json';
 import { Bullseye, Spinner } from '@patternfly/react-core';
-const Groups = React.lazy(() =>
-  import(/* webpackChunkName: "GroupsPage" */ './Routes/Groups/Groups')
-);
-const GroupsDetail = React.lazy(() =>
-  import(
-    /* webpackChunkName: "GroupsDetailPage" */ './Routes/GroupsDetail/GroupsDetail'
-  )
-);
+// const Groups = React.lazy(() =>
+//   import(/* webpackChunkName: "GroupsPage" */ './Routes/Groups/Groups')
+// );
+// const GroupsDetail = React.lazy(() =>
+//   import(
+//     /* webpackChunkName: "GroupsDetailPage" */ './Routes/GroupsDetail/GroupsDetail'
+//   )
+// );
 
 const DeviceDetail = React.lazy(() =>
   import(
@@ -18,11 +18,11 @@ const DeviceDetail = React.lazy(() =>
   )
 );
 
-const Canaries = React.lazy(() =>
-  import(
-    /* webpackChunkName: "GroupsDetailPage" */ './Routes/Canaries/Canaries'
-  )
-);
+// const Canaries = React.lazy(() =>
+//   import(
+//     /* webpackChunkName: "GroupsDetailPage" */ './Routes/Canaries/Canaries'
+//   )
+// );
 
 const Devices = React.lazy(() =>
   import(/* webpackChunkName: "GroupsDetailPage" */ './Routes/Devices/Devices')
@@ -38,14 +38,14 @@ export const Routes = () => {
       }
     >
       <Switch>
-        <Route exact path={paths.groups} component={Groups} />
-        <Route exact path={paths['groups-detail']} component={GroupsDetail} />
-        <Route path={paths['device-detail']} component={DeviceDetail} />
-        <Route path={paths.canaries} component={Canaries} />
+        {/* <Route exact path={paths.groups} component={Groups} /> */}
+        {/* <Route exact path={paths['groups-detail']} component={GroupsDetail} /> */}
+        {/* <Route path={paths['device-detail']} component={DeviceDetail} /> */}
+        {/* <Route path={paths.canaries} component={Canaries} /> */}
         <Route exact path={paths.devices} component={Devices} />
         <Route path={paths['devices-detail']} component={DeviceDetail} />
         <Route>
-          <Redirect to={paths.groups} />
+          <Redirect to={paths.devices} />
         </Route>
       </Switch>
     </Suspense>
