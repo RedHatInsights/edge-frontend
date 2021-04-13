03f40a82a55a363143a2d9f20a7f470db9775f69
boaz0 <ripcurld.github@gmail.com>
2021-04-13 19:10:45 +0300

    Disable http in SPANDX + remove server:ctr from package.json (#27)
    Signed-off-by: Boaz Shuster <boaz.shuster.github@gmail.com>

--- profiles/local-frontend.js
+++ profiles/local-frontend.js
@@ -6,12 +6,12 @@ const FRONTEND_PORT = 8002;
 const routes = {};
 
 routes[`/beta/${SECTION ? `/${section}` : ''}${APP_ID}`] = {
-  host: `https://localhost:${FRONTEND_PORT}`,
+  host: `http://localhost:${FRONTEND_PORT}`,
 };
 routes[`/${SECTION ? `/${section}` : ''}${APP_ID}`] = {
-  host: `https://localhost:${FRONTEND_PORT}`,
+  host: `http://localhost:${FRONTEND_PORT}`,
 };
-routes[`/beta/apps/${APP_ID}`] = { host: `https://localhost:${FRONTEND_PORT}` };
-routes[`/apps/${APP_ID}`] = { host: `https://localhost:${FRONTEND_PORT}` };
+routes[`/beta/apps/${APP_ID}`] = { host: `http://localhost:${FRONTEND_PORT}` };
+routes[`/apps/${APP_ID}`] = { host: `http://localhost:${FRONTEND_PORT}` };
 
 module.exports = { routes };
