aa13720d1c57d0f1806299c05bb7c01744bab0a4
csantiago <resolutecoder@gmail.com>
2021-06-24 15:07:33 -0500

    updated review step page in wizard, created ReviewSection component

---
+++ src/components/ReviewSection.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { 
+  Text, 
+  Grid,
+  GridItem,
+  TextListItem,
+  TextVariants,
+  TextListItemVariants,
+} from '@patternfly/react-core';
+
+const ReviewSection = ({ title, data, testid }) => {
+  return (
+    <Grid
+      data-testid={testid}
+      hasGutter
+    >
+      <GridItem span={12} hasGutter>
+        <Text component={TextVariants.h1}>{title}</Text> 
+      </GridItem>
+      {data.map(({ name, value }) => 
+        <>
+          <GridItem span={3} hasGutter>
+            <TextListItem component={TextListItemVariants.dt}>{name}</TextListItem>
+          </GridItem>
+          <GridItem style={{ paddingBottom: '20px' }} span={9} hasGutter>
+            <TextListItem component={TextListItemVariants.dd}>{value}</TextListItem>
+          </GridItem>
+        </>
+      )}
+    </Grid>
+  )
+}
+
+export default ReviewSection;
