2f70bcac62e99592f9eca362355a3907f5adaa91
boaz0 <boaz.shuster.github@gmail.com>
2021-07-06 17:59:35 +0300

    Update local-frontend-and-api config to run against local api (#73)
    Signed-off-by: Boaz Shuster <boaz.shuster.github@gmail.com>

--- profiles/local-frontend-and-api.js
+++ profiles/local-frontend-and-api.js
@@ -3,18 +3,20 @@
 const SECTION = '';
 const APP_ID = 'edge';
 const FRONTEND_PORT = 8002;
-const API_PORT = 8888;
+const API_PORT = 3000;
 const routes = {};
 
-routes[`/beta/${SECTION}/${APP_ID}`] = {
-  host: `https://localhost:${FRONTEND_PORT}`,
+routes[`/beta/${SECTION ? `/${section}` : ''}${APP_ID}`] = {
+  host: `http://localhost:${FRONTEND_PORT}`,
 };
-routes[`/${SECTION}/${APP_ID}`] = {
-  host: `https://localhost:${FRONTEND_PORT}`,
+routes[`/${SECTION ? `/${section}` : ''}${APP_ID}`] = {
+  host: `http://localhost:${FRONTEND_PORT}`,
 };
-routes[`/beta/apps/${APP_ID}`] = { host: `https://localhost:${FRONTEND_PORT}` };
-routes[`/apps/${APP_ID}`] = { host: `https://localhost:${FRONTEND_PORT}` };
+routes[`/beta/apps/${APP_ID}`] = { host: `http://localhost:${FRONTEND_PORT}` };
+routes[`/apps/${APP_ID}`] = { host: `http://localhost:${FRONTEND_PORT}` };
 
-routes[`/api/${APP_ID}`] = { host: `https://localhost:${API_PORT}` };
+routes[`/api/${APP_ID}`] = { host: `http://localhost:${API_PORT}` };
+
+routes['/config'] = { host: 'http://localhost:8889' }
 
 module.exports = { routes };
