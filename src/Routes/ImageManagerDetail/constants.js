c8e7784e3fe7bddc3a5376e91b6bbd5c078487c4
matoval <mat24c@gmail.com>
2021-07-09 08:12:56 -0700

    added rhel-84 to the long value and added rhel-84-short for the short value

--- src/Routes/ImageManagerDetail/constants.js
+++ src/Routes/ImageManagerDetail/constants.js
@@ -30,8 +30,8 @@ export const imageStatusMapper = {
 };
 
 export const releaseMapper = {
-  'rhel-8': 'Red Hat Enterprise Linux (RHEL) 8.4',
-  'rhel-84': 'RHEL 8.4',
+  'rhel-84': 'Red Hat Enterprise Linux (RHEL) 8.4',
+  'rhel-84-short': 'RHEL 8.4',
 };
 
 export const imageTypeMapper = {
