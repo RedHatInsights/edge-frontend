1bcf0d62d4bb29841336631014f54fd546cc4143
Boaz Shuster <boaz.shuster.github@gmail.com>
2021-07-13 15:48:06 +0300

    Move notification reducer to App.js (#80)
    Signed-off-by: Boaz Shuster <boaz.shuster.github@gmail.com>

--- src/components/Tiles.js
+++ src/components/Tiles.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useContext } from 'react';
 import { Flex, FlexItem } from '@patternfly/react-core';
 import DeviceSummaryTile from './DeviceSummaryTile';
-import { notificationsReducer } from '@redhat-cloud-services/frontend-components-notifications/redux';
 import { imagesReducer, deviceSummaryReducer } from '../store/reducers';
 import { useDispatch } from 'react-redux';
 import { loadImages, loadDeviceSummary } from '../store/actions';
@@ -15,7 +14,6 @@ export const Tiles = () => {
     const registered = getRegistry().register({
       imagesReducer,
       deviceSummaryReducer,
-      notifications: notificationsReducer,
     });
     loadImages(dispatch);
     loadDeviceSummary(dispatch);
