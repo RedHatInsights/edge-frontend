4d27cd64a42524240063fe3fa9640dd8a9760d26
csantiago <resolutecoder@gmail.com>
2021-08-04 14:41:01 -0500

    added warning text helper to installer and styles to output step

--- src/components/form/ImageOutputCheckbox.js
+++ src/components/form/ImageOutputCheckbox.js
@@ -5,18 +5,37 @@ import {
   TextContent,
   Text,
   TextVariants,
+  FormHelperText,
 } from '@patternfly/react-core';
 import useFieldApi from '@data-driven-forms/react-form-renderer/use-field-api';
 import PropTypes from 'prop-types';
+import useFormApi from '@data-driven-forms/react-form-renderer/use-form-api';
+import { ExclamationTriangleIcon } from '@patternfly/react-icons';
+import warningColor from '@patternfly/react-tokens/dist/esm/global_warning_color_100';
+
+const WarningInstallerHelperText = () => (
+  <FormHelperText
+    isHidden={false}
+    className='pf-u-ml-xl'
+    icon={<ExclamationTriangleIcon color={warningColor.value} />}
+  >
+    Creating an installable version of your image increases the build time and
+    is not needed for updating existing devices. You can create an installable
+    version of this image later if you continue with this option
+  </FormHelperText>
+);
 
 const outputHelperText = {
   'rhel-edge-commit':
     'An OSTree commit is always created when building an image.',
   'rhel-edge-installer':
     'An installable version of the image is typically created with a brand new image.',
+  'rhel-edge-installer-warning': `Creating an installable version of your image increases the build time  \
+  and is not needed for updating existing devices. \n You can create an installable version of this image later if you continue with this option`,
 };
 
 const ImageOutputCheckbox = (props) => {
+  const { getState } = useFormApi();
   const { input } = useFieldApi(props);
   const toggleCheckbox = useCallback(
     (checked, event) => {
@@ -31,7 +50,7 @@ const ImageOutputCheckbox = (props) => {
 
   return (
     <FormGroup
-      label="Output type"
+      label='Output type'
       isHelperTextBeforeField
       hasNoPaddingTop
       isRequired
@@ -49,11 +68,21 @@ const ImageOutputCheckbox = (props) => {
           />
           <TextContent>
             <Text component={TextVariants.small}>
-              {outputHelperText[value]}
+              {getState()?.initialValues?.isUpdate &&
+              value === 'rhel-edge-installer' ? (
+                <WarningInstallerHelperText />
+              ) : (
+                outputHelperText[value]
+              )}
             </Text>
             {value === 'rhel-edge-installer' && (
-              <Text component={TextVariants.p}>
-                <Text component={TextVariants.a} isVisitedLink href="#">
+              <Text component={TextVariants.small}>
+                <Text
+                  className='pf-u-ml-xl'
+                  component={TextVariants.a}
+                  isVisitedLink
+                  href='#'
+                >
                   Learn more about image types.
                 </Text>
               </Text>
