43c562db4088fb32ff47e5c4aa071b26e867f401
matoval <mat24c@gmail.com>
2021-09-20 17:06:01 -0700

    lint

--- src/store/polling.js
+++ src/store/polling.js
@@ -6,47 +6,45 @@ const hasPollingData = (polling = {}) => {
 
 const ofPollingType = (type) => type.endsWith(`_${POLLING_SUFFIX}`);
 
-export const polling =
-  (pollHash = {}) =>
-  ({ dispatch }) =>
-  (next) =>
-  (action) => {
-    const { type, payload, meta } = action;
-    if (ofPollingType(type) && hasPollingData(payload)) {
-      const { id, fetcher, condition, onEvent } = payload;
-      if (pollHash[id]) {
-        return;
-      }
-      pollHash[id] = true;
-      setTimeout(() => {
-        dispatch({
-          type: id,
-          payload: fetcher().then((resp) => {
-            pollHash[id] = false;
-            const [toContinue, stateName] = condition(resp);
-            const nextActions = onEvent?.[stateName] || [];
-            nextActions.forEach((action) => action(dispatch));
-            if (toContinue) {
-              dispatch(action);
-            }
-          }),
-        }).catch(() => null);
-      }, 15 * 1000);
+export const polling = (pollHash = {}) => ({ dispatch }) => (next) => (
+  action
+) => {
+  const { type, payload, meta } = action;
+  if (ofPollingType(type) && hasPollingData(payload)) {
+    const { id, fetcher, condition, onEvent } = payload;
+    if (pollHash[id]) {
       return;
     }
-
-    if (hasPollingData(meta?.polling)) {
-      const { id, fetcher, condition, onEvent } = meta.polling;
+    pollHash[id] = true;
+    setTimeout(() => {
       dispatch({
-        type: `${id}_POLLING`,
-        payload: {
-          id,
-          fetcher,
-          condition,
-          onEvent,
-        },
-      });
-    }
+        type: id,
+        payload: fetcher().then((resp) => {
+          pollHash[id] = false;
+          const [toContinue, stateName] = condition(resp);
+          const nextActions = onEvent?.[stateName] || [];
+          nextActions.forEach((action) => action(dispatch));
+          if (toContinue) {
+            dispatch(action);
+          }
+        }),
+      }).catch(() => null);
+    }, 15 * 1000);
+    return;
+  }
 
-    next(action);
-  };
+  if (hasPollingData(meta?.polling)) {
+    const { id, fetcher, condition, onEvent } = meta.polling;
+    dispatch({
+      type: `${id}_POLLING`,
+      payload: {
+        id,
+        fetcher,
+        condition,
+        onEvent,
+      },
+    });
+  }
+
+  next(action);
+};
