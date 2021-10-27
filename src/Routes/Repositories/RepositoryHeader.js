f9bcd8ef0b0c763511c295f3ca9d4ab24b30802a
matoval <mat24c@gmail.com>
2021-10-26 20:18:52 -0700

    lint

--- src/Routes/Repositories/RepositoryHeader.js
+++ src/Routes/Repositories/RepositoryHeader.js
@@ -8,7 +8,7 @@ import { ExternalLinkAltIcon } from '@patternfly/react-icons';
 
 const RepositoryHeader = () => {
   return (
-    <PageHeader data-testid="repository-header-title" >
+    <PageHeader data-testid="repository-header-title">
       <>
         <PageHeaderTitle title="Applications settings" />
         <p data-testid="repository-header-sub-title" className="pf-u-mt-sm">
@@ -16,14 +16,23 @@ const RepositoryHeader = () => {
           <Popover
             aria-label="Basic popover"
             className="pf-ml-0"
-            headerContent={<div data-testid="repository-header-popover-title" >About Fleet Management</div>}
+            headerContent={
+              <div data-testid="repository-header-popover-title">
+                About Fleet Management
+              </div>
+            }
             bodyContent={
-              <div data-testid="repository-header-popover-body" >
-                Fleet Management is a service that allows you to provision, update and maintain edge systems.
+              <div data-testid="repository-header-popover-body">
+                Fleet Management is a service that allows you to provision,
+                update and maintain edge systems.
               </div>
             }
             footerContent={
-              <Button data-testid="repository-header-popover-footer" variant="link" style={{ 'padding-left': 0 }}>
+              <Button
+                data-testid="repository-header-popover-footer"
+                variant="link"
+                style={{ 'padding-left': 0 }}
+              >
                 <a href="#">Documentation</a>
                 <ExternalLinkAltIcon className="pf-u-ml-sm" />
               </Button>
--- src/Routes/Repositories/RepositoryHeader.test.js
+++ src/Routes/Repositories/RepositoryHeader.test.js
@@ -4,23 +4,31 @@ import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 
 describe('Repository Header', () => {
   it('should render correctly', async () => {
-    
-    render(
-      <RepositoryHeader />
-    )
+    render(<RepositoryHeader />);
 
     const title = screen.getByTestId('repository-header-title');
     const subTitle = screen.getByTestId('repository-header-sub-title');
     const questionIcon = screen.getByTestId('repository-header-popover-btn');
 
-    expect(title.firstChild.innerHTML).toEqual(' Applications settings ')
-    expect(subTitle.firstChild.textContent).toEqual('Settings for Fleet Management')
-    expect(questionIcon).toBeDefined()
-    fireEvent.click(questionIcon)
+    expect(title.firstChild.innerHTML).toEqual(' Applications settings ');
+    expect(subTitle.firstChild.textContent).toEqual(
+      'Settings for Fleet Management'
+    );
+    expect(questionIcon).toBeDefined();
+    fireEvent.click(questionIcon);
     await waitFor(() => {
-      expect(screen.getByTestId('repository-header-popover-title').innerHTML).toEqual('About Fleet Management')
-      expect(screen.getByTestId('repository-header-popover-body').innerHTML).toEqual('Fleet Management is a service that allows you to provision, update and maintain edge systems.')
-      expect(screen.getByTestId('repository-header-popover-footer').firstChild.innerHTML).toEqual('Documentation')
-    })
-  })
-})
+      expect(
+        screen.getByTestId('repository-header-popover-title').innerHTML
+      ).toEqual('About Fleet Management');
+      expect(
+        screen.getByTestId('repository-header-popover-body').innerHTML
+      ).toEqual(
+        'Fleet Management is a service that allows you to provision, update and maintain edge systems.'
+      );
+      expect(
+        screen.getByTestId('repository-header-popover-footer').firstChild
+          .innerHTML
+      ).toEqual('Documentation');
+    });
+  });
+});
