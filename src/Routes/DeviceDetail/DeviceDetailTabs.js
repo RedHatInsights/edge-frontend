ba42f1522d32349aa75be72cb4c8f63275b3de25
matoval <mat24c@gmail.com>
2021-11-11 13:33:11 -0800

    Add vulerability tab to device details

--- src/Routes/DeviceDetail/DeviceDetailTabs.js
+++ src/Routes/DeviceDetail/DeviceDetailTabs.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Tabs, Tab, TabTitleText } from '@patternfly/react-core';
 import { AppInfo } from '@redhat-cloud-services/frontend-components/Inventory';
+import VulnerabilityTab from './Vulnerability';
 
 const DeviceDetailTabs = () => {
   const [activeTabKey, setActiveTabkey] = useState(0);
@@ -14,6 +15,9 @@ const DeviceDetailTabs = () => {
       >
         <Tab eventKey={0} title={<TabTitleText>Details</TabTitleText>}>
           <AppInfo showTags fallback="" />
+        </Tab>
+        <Tab eventKey={1} title={<TabTitleText>Vulnerability</TabTitleText>}>
+          <VulnerabilityTab />
         </Tab>
       </Tabs>
     </div>
