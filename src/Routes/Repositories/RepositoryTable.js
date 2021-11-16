3847dd20119e192aae60ec2e74dfeddc1f226a38
chris <chris@resolutecoder>
2021-11-16 16:58:13 -0600

    custom repo link opens in new tab

--- src/Routes/Repositories/RepositoryTable.js
+++ src/Routes/Repositories/RepositoryTable.js
@@ -45,7 +45,9 @@ const RepositoryTable = ({ data, openModal }) => {
                 {name}
               </Text>
               <Text component={TextVariants.a}>
-                <a href={baseURL}>{baseURL}</a>{' '}
+                <a href={baseURL} target="_blank" rel="noopener noreferrer">
+                  {baseURL}
+                </a>{' '}
                 <ExternalLinkAltIcon classname="pf-u-ml-sm" />
               </Text>
             </>
