0128091402bbfb24bcb2592ee439b2d3463e35f3
matoval <mat24c@gmail.com>
2021-12-03 16:03:22 -0800

    Add test for successful login

--- cypress/integration/smoke/login.spec.js
+++ cypress/integration/smoke/login.spec.js
@@ -63,6 +63,21 @@ describe('simple login to fleet management', () => {
         const userPassword = Cypress.env('stage_password')
 
         cy.waitFor('#username-verification')
+        cy.get('#username-verification').type(`${wrongUserName}{enter}`)
+        cy.waitFor('#password')
+        cy.get('#password').type(`${wrongUserPassword}{enter}`)
+        cy.get('#rh-login-form-error-title')
+            .should('contain', failedLogin)
+    })
+
+    it('login successfully', () => {
+        // We use the `cy.get()` command to get all elements that match the selector.
+        // Then, we use `should` to assert that there are two matched items,
+        // which are the two default items.
+        const userName = Cypress.env('stage_user')
+        const userPassword = Cypress.env('stage_password')
+
+        cy.waitFor('#username-verification')
         cy.get('#username-verification').type(`${userName}{enter}`)
         cy.waitFor('#password')
         cy.get('#password').type(`${userPassword}{enter}`)
