fdedf17284c884d54d29dc98d24f9d8d538f4bee
Og B. Maciel <omaciel@ogmaciel.com>
2021-12-09 13:13:43 -0500

    Allow Cypress to use different config files.
    This PR adds support for passing different configuration files to Cypress.
    
    Configuration files can be added under the `cypress/config` directory and
    should be named using the naming format `<ENV_NAME>.env.js`. If none is
    provided, then cypress will attempt to load `cypress/config/local.env.js` as
    the default.
    
    Signed-off-by: Og B. Maciel <omaciel@ogmaciel.com>

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -16,7 +16,19 @@
  * @type {Cypress.PluginConfig}
  */
 // eslint-disable-next-line no-unused-vars
+const fs = require('fs-extra');
+const path = require('path');
+
+function getConfigurationByFile(file) {
+    const pathToConfigFile = path.resolve('cypress/config', `${file}.env.json`);
+
+    return fs.readJson(pathToConfigFile)
+}
+
+// plugins file
 module.exports = (on, config) => {
-  // `on` is used to hook into various events Cypress emits
-  // `config` is the resolved Cypress config
+    // accept a configFile value or use local by default
+    const file = config.env.configFile || 'local';
+
+    return getConfigurationByFile(file)
 }
