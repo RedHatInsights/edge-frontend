67561dce7db3efcdafcd636cc8a1a5b8c72de2b8
matoval <mat24c@gmail.com>
2022-01-19 17:39:33 -0800

    lint

--- src/Routes/Repositories/RepositoryHeader.test.js
+++ src/Routes/Repositories/RepositoryHeader.test.js
@@ -1,18 +1,29 @@
-import React from "react";
-import { fireEvent, render, screen, waitFor } from "@testing-library/react"
-import RepositoryHeader from "./RepositoryHeader";
+import React from 'react';
+import { fireEvent, render, screen, waitFor } from '@testing-library/react';
+import RepositoryHeader from './RepositoryHeader';
 
 describe('RepositoryHeader', () => {
   it('renders correctly', async () => {
-    
-    render(<RepositoryHeader />)
+    render(<RepositoryHeader />);
 
-    expect(screen.getByRole('heading', 'Applications settings')).toBeDefined()
-    expect(screen.getByText('Settings for Fleet Management')).toBeDefined()
-    expect(() => screen.getByText('About Fleet Management')).toThrow()
-    expect(() => screen.getByText('Fleet Management is a service that allows you to provision, update and maintain edge systems.')).toThrow()
-    fireEvent.click(screen.getByTestId('test-pop-over'))
-    waitFor(() => expect(screen.getByText('About Fleet Management')).toBeDefined())
-    waitFor(() => expect(screen.getByText('Fleet Management is a service that allows you to provision, update and maintain edge systems.')).toBeDefined())
-  })
-})
+    expect(screen.getByRole('heading', 'Applications settings')).toBeDefined();
+    expect(screen.getByText('Settings for Fleet Management')).toBeDefined();
+    expect(() => screen.getByText('About Fleet Management')).toThrow();
+    expect(() =>
+      screen.getByText(
+        'Fleet Management is a service that allows you to provision, update and maintain edge systems.'
+      )
+    ).toThrow();
+    fireEvent.click(screen.getByTestId('test-pop-over'));
+    waitFor(() =>
+      expect(screen.getByText('About Fleet Management')).toBeDefined()
+    );
+    waitFor(() =>
+      expect(
+        screen.getByText(
+          'Fleet Management is a service that allows you to provision, update and maintain edge systems.'
+        )
+      ).toBeDefined()
+    );
+  });
+});
--- src/components/Empty.test.js
+++ src/components/Empty.test.js
@@ -1,33 +1,35 @@
-import React from "react";
-import { render, screen } from "@testing-library/react"
-import Empty from "./Empty";
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import Empty from './Empty';
 
 describe('Empty', () => {
   it('renders correctly', () => {
     const primaryAction = {
       text: 'Primary action',
-      click: () => jest.fn()
-    }
+      click: () => jest.fn(),
+    };
 
     const secondaryActions = [
       {
         title: 'Secondary action',
         type: 'link',
-        link: '#'
-      }
-    ]
+        link: '#',
+      },
+    ];
 
-    render(<Empty
-      icon="repository"
-      title="Test empty state title"
-      body="Test empty state body"
-      primaryAction={primaryAction}
-      secondaryActions={secondaryActions}
-    />)
+    render(
+      <Empty
+        icon="repository"
+        title="Test empty state title"
+        body="Test empty state body"
+        primaryAction={primaryAction}
+        secondaryActions={secondaryActions}
+      />
+    );
 
-    expect(screen.getByRole('heading','Test empty state title')).toBeDefined()
-    expect(screen.getByRole('button','Primary action')).toBeDefined()
-    expect(screen.getByRole('link','Secondary action')).toBeDefined()
-    expect(screen.getByText('Test empty state body')).toBeDefined()
-  })
-})
+    expect(screen.getByRole('heading', 'Test empty state title')).toBeDefined();
+    expect(screen.getByRole('button', 'Primary action')).toBeDefined();
+    expect(screen.getByRole('link', 'Secondary action')).toBeDefined();
+    expect(screen.getByText('Test empty state body')).toBeDefined();
+  });
+});
