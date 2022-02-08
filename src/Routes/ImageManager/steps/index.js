3babb0a66b03b50eca05bdac28751e90ff73e3a8
Adelia Ferreira <tay.figueira@gmail.com>
2022-02-08 14:59:41 +0000

    Added Skeletom to strucuture on wizard
    Signed-off-by: Adelia Ferreira <tay.figueira@gmail.com>

--- src/Routes/ImageManager/steps/index.js
+++ src/Routes/ImageManager/steps/index.js
@@ -4,3 +4,4 @@ export { default as imageOutput } from './imageOutput';
 export { default as imageSetDetails } from './imageSetDetails';
 export { default as updateDetails } from './updateDetails';
 export { default as registration } from './registration';
+export { default as repositories } from './repositories';
