f7a6c8199e137d438d60ee0fa40b7bb94a24b69f
Chris Santiago <50929884+resoluteCoder@users.noreply.github.com>
2022-02-10 17:52:41 -0600

    Update Groups.js

--- src/Routes/Groups/Groups.js
+++ src/Routes/Groups/Groups.js
@@ -8,7 +8,6 @@ import { Main } from '@redhat-cloud-services/frontend-components/Main';
 import GroupTable from './GroupTable';
 import Empty from '../../components/Empty';
 import Modal from '../../components/Modal';
-import { Link } from 'react-router-dom';
 import { getGroups } from '../../api/index';
 import { createGroup, updateGroupById } from '../../api/index';
 import validatorTypes from '@data-driven-forms/react-form-renderer/validator-types';
@@ -54,18 +53,7 @@ const Groups = () => {
   return (
     <>
       <PageHeader className="pf-m-light">
-        <Flex justifyContent={{ default: 'justifyContentSpaceBetween' }}>
-          <FlexItem>
-            <PageHeaderTitle title="Groups" />
-          </FlexItem>
-          <FlexItem>
-            <Button variant="secondary">
-              <Link style={{ textDecoration: 'none' }} to="/inventory">
-                View entire inventory
-              </Link>
-            </Button>
-          </FlexItem>
-        </Flex>
+        <PageHeaderTitle title="Groups" />
       </PageHeader>
       <Main className="edge-devices">
         {isLoading ? (
