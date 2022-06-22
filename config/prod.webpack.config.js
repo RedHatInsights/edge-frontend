ca0bda169a42a967e20bd2d12b6e08eb56daae10
matoval <mat24c@gmail.com>
2022-06-22 12:28:22 -0700

    Add local prod:proxy env

--- config/prod.webpack.config.js
+++ config/prod.webpack.config.js
@@ -1,8 +1,36 @@
 const { resolve } = require('path');
 const config = require('@redhat-cloud-services/frontend-components-config');
+
+const insightsProxy = {
+  https: false,
+  ...(process.env.BETA && { deployment: 'beta/apps' }),
+};
+
+const webpackProxy = {
+  deployment: process.env.BETA ? 'beta/apps' : 'apps',
+  useProxy: true,
+  env: `${process.env.ENVIRONMENT || 'prod'}-${
+    process.env.BETA ? 'beta' : 'stable'
+  }`, // for accessing prod-beta start your app with ENVIRONMENT=prod and BETA=true
+  appUrl: process.env.BETA ? '/beta/edge' : '/edge',
+  routes: {
+    ...(process.env.API_PORT && {
+      '/api/edge': { host: `http://localhost:${process.env.API_PORT}` },
+    }),
+    ...(process.env.CONFIG_PORT && {
+      [`${process.env.BETA ? '/beta' : ''}/config`]: {
+        host: `http://localhost:${process.env.CONFIG_PORT}`,
+      },
+    }),
+  },
+};
+
 const { config: webpackConfig, plugins } = config({
   rootFolder: resolve(__dirname, '../'),
+  debug: true,
+  useFileHash: false,
   sassPrefix: '.fleet-management, .edge',
+  ...(process.env.PROXY ? webpackProxy : insightsProxy),
 });
 
 plugins.push(
@@ -13,7 +41,7 @@ plugins.push(
   )
 );
 
-// plugins.push(new (require('webpack-bundle-analyzer').BundleAnalyzerPlugin)());
+webpackConfig.devServer.client.overlay = false;
 
 module.exports = {
   ...webpackConfig,
