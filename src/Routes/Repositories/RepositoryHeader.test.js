f39620f17f1a807d56a743aa5f77f390f86698e8
resolutecoder <resolutecoder@gmail.com>
2022-06-24 09:22:52 -0500

    fixed add modal button for empty state repo

--- src/Routes/Repositories/RepositoryHeader.test.js
+++ src/Routes/Repositories/RepositoryHeader.test.js
@@ -1,31 +1,17 @@
 import React from 'react';
-import { fireEvent, render, screen, waitFor } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import RepositoryHeader from './RepositoryHeader';
 
 describe('RepositoryHeader', () => {
   it('renders correctly', async () => {
     const { container } = render(<RepositoryHeader />);
 
-    expect(screen.getByRole('heading', 'Applications settings')).toBeDefined();
-    expect(screen.getByText('Settings for Edge Management')).toBeDefined();
-    expect(() => screen.getByText('About Edge Management')).toThrow();
-    expect(() =>
+    expect(screen.getByRole('heading', 'Custom repositories')).toBeDefined();
+    expect(
       screen.getByText(
-        'Edge Management is a service that allows you to provision, update and maintain edge systems.'
+        'Add custom repositories to build RHEL for Edge images with additional packages.'
       )
-    ).toThrow();
-    fireEvent.click(screen.getByTestId('test-pop-over'));
-    await waitFor(() =>
-      expect(screen.getByText('About Edge Management')).toBeDefined()
-    );
-    await waitFor(() =>
-      expect(
-        screen.getByText(
-          'Edge Management is a service that allows you to provision, update and maintain edge systems.'
-        )
-      ).toBeDefined()
-    );
-
+    ).toBeDefined();
     expect(container.querySelector('div')).toMatchSnapshot();
   });
 });
