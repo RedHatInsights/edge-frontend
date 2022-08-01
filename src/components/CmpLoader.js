b270a418de39096f988cd9d2f9807775a609aed2
Maeve Hoffer <dhoffer@redhat.com>
2022-08-01 09:28:12 -0700

    add return statement to CmpLoader

--- src/components/CmpLoader.js
+++ src/components/CmpLoader.js
@@ -3,16 +3,14 @@ import { Skeleton } from '@patternfly/react-core';
 
 const CmpLoader = () => {
   return (
-    <>
-      <React.Fragment>
-        <Skeleton />
-        <br />
-        <Skeleton />
-        <br />
-        <Skeleton />
-        <br />
-      </React.Fragment>
-    </>
+    <React.Fragment>
+      <Skeleton />
+      <br />
+      <Skeleton />
+      <br />
+      <Skeleton />
+      <br />
+    </React.Fragment>
   );
 };
 
