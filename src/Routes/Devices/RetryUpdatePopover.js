4b13bc39b66b3490e607d6ccb4cddc1e7ba9a3fa
Demetrius Lima <delima@redhat.com>
2022-09-20 16:56:51 -0300

    Fix unresponsive message and one uppercase

--- src/Routes/Devices/RetryUpdatePopover.js
+++ src/Routes/Devices/RetryUpdatePopover.js
@@ -21,7 +21,7 @@ function getDevicePopoverDescription(props) {
       <div>
         The playbook failed to run. You can retry the update or build a new one.
         <Stack className="pf-u-mt-sm">
-          <StackItem className="pf-u-font-weight-bold">Last Seen</StackItem>
+          <StackItem className="pf-u-font-weight-bold">Last seen</StackItem>
           <StackItem> {<DateFormat date={props.lastSeen} />}</StackItem>
         </Stack>
       </div>
@@ -36,7 +36,7 @@ function getDevicePopoverDescription(props) {
         The service timed out during the last update. You can retry the update
         or build a new one.
         <Stack className="pf-u-mt-sm">
-          <StackItem className="pf-u-font-weight-bold">Last Seen</StackItem>
+          <StackItem className="pf-u-font-weight-bold">Last seen</StackItem>
           <StackItem> {<DateFormat date={props.lastSeen} />}</StackItem>
         </Stack>
       </div>
@@ -47,8 +47,8 @@ function getDevicePopoverDescription(props) {
     return (
       <div>
         The service could not be reached via RHC. The device may communicate at
-        a later time if this is a network issues or could be an indication of a
-        larger problem.
+        a later time if this is a network issue or could be an indication of a
+        more significant problem.
         <Stack className="pf-u-mt-sm">
           <StackItem className="pf-u-font-weight-bold">Last Seen</StackItem>
           <StackItem> {<DateFormat date={props.lastSeen} />}</StackItem>
