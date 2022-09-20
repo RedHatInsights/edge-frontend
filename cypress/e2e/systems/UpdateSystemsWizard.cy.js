df7a873b51de8c4fb675feabbc793426a82e6a76
Maeve Hoffer <dhoffer@redhat.com>
2022-09-20 14:31:52 -0700

    add intercepts to allow test to ensure update modal sends correct information

--- cypress/e2e/systems/UpdateSystemsWizard.cy.js
+++ cypress/e2e/systems/UpdateSystemsWizard.cy.js
@@ -9,6 +9,41 @@ describe("Systems", () => {
     cy.intercept("GET", "/api/edge/v1/devices/devicesview*", {
       fixture: "systemTable.json",
     });
+
+    cy.intercept("POST", "/api/edge/v1/updates*", (req) => {
+      expect(req.body.hasOwnProperty("DevicesUUID")).to.be.true;
+      expect(req.body.DevicesUUID).to.deep.eq(["testdevice2", "testdevice3"]);
+      req.reply({});
+    });
+
+    cy.intercept("GET", "/api/edge/v1/image-sets/1*", {
+      Count: 1,
+      Data: {
+        image_set: {
+          ID: 1,
+          Name: "dl-image-85",
+          Version: 2,
+        },
+        images: [
+          {
+            image: {
+              ID: 1,
+              CreatedAt: "2022-08-17T11:14:04.230226Z",
+              Name: "dl-image-85",
+              Distribution: "rhel-85",
+              Version: 2,
+              CommitID: 878,
+              ImageSetID: 1,
+            },
+            update_added: 116,
+            update_removed: 0,
+            update_updated: 0,
+          },
+        ],
+        image_build_iso_url: "",
+      },
+    });
+
     cy.get(".pf-c-title", { timeout: 30000 }).should("include.text", "Systems");
 
     //Update button disabled on open/when no devices are selected
@@ -56,12 +91,7 @@ describe("Systems", () => {
       .contains("Update")
       .should("to.not.be.disabled");
 
-    //Deselect second device
-    cy.get(
-      '[data-ouia-component-id="OUIA-Generated-TableRow-3"] > .pf-c-table__check > input'
-    ).click();
-
-    //Select fourth device, which has different image than third
+    //Select fourth device, which has different image
     cy.get(
       '[data-ouia-component-id="OUIA-Generated-TableRow-5"] > .pf-c-table__check > input'
     ).click();
@@ -70,6 +100,71 @@ describe("Systems", () => {
     cy.get(".pf-c-button.pf-m-primary")
       .contains("Update")
       .should("to.be.disabled");
+
+    //Deselect fourth device
+    cy.get(
+      '[data-ouia-component-id="OUIA-Generated-TableRow-5"] > .pf-c-table__check > input'
+    ).click();
+
+    const updateDevices = [
+      {
+        DeviceID: 164010,
+        DeviceName: "maeve-test-device2",
+        DeviceUUID: "a921f9cf-8042-46a8-9b3c-0f3d2fc85cfb",
+        ImageID: 27170,
+        ImageName: "iqe-test-image-8_5",
+        LastSeen: "2022-09-14T20:06:39.584623Z",
+        UpdateAvailable: true,
+        Status: "RUNNING",
+        ImageSetID: 8102,
+        DeviceGroups: [
+          {
+            ID: 52760,
+            Name: "test_device_groups_api_LmGwoYi7dz88OJ9Im",
+          },
+        ],
+        DispatcherStatus: "",
+        DispatcherReason: "",
+      },
+      {
+        DeviceID: 164011,
+        DeviceName: "maeve-test-device3",
+        DeviceUUID: "c921f9cf-8042-46a8-9b3c-0f3d2fc85cfb",
+        ImageID: 27170,
+        ImageName: "iqe-test-image-8_5",
+        LastSeen: "2022-09-14T20:06:39.584623Z",
+        UpdateAvailable: true,
+        Status: "RUNNING",
+        ImageSetID: 8102,
+        DeviceGroups: [
+          {
+            ID: 52760,
+            Name: "test_device_groups_api_LmGwoYi7dz88OJ9Im",
+          },
+        ],
+      },
+    ];
+
+    //Click button while enabled
+    cy.get(".pf-c-button.pf-m-primary")
+      .contains("Update")
+      .should("to.not.be.disabled")
+      .click();
+
+    cy.wait(3000);
+
+    cy.get(".pf-c-modal-box__title-text").should(
+      "include.text",
+      "Update systems to latest image version"
+    );
+
+    cy.get(".pf-c-form__actions > .pf-m-primary")
+      .contains("Update system")
+      .click();
+
+    cy.get(".pf-c-alert__title").should(
+      "include.text",
+      "Info alert:Updating system"
+    );
   });
 });
-//});
