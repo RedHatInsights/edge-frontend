89846ecaccbfbf2b6abddaf207c1580da39b6b43
matoval <mat24c@gmail.com>
2022-09-22 17:33:31 -0700

    disable update if no systems have a status of updateAvailabe || error

--- cypress/e2e/systems/UpdateSystemsWizard.cy.js
+++ cypress/e2e/systems/UpdateSystemsWizard.cy.js
@@ -82,45 +82,6 @@ describe("Systems", () => {
       '[data-ouia-component-id="OUIA-Generated-TableRow-5"] > .pf-c-table__check > input'
     ).click();
 
-    const updateDevices = [
-      {
-        DeviceID: 164010,
-        DeviceName: "maeve-test-device2",
-        DeviceUUID: "a921f9cf-8042-46a8-9b3c-0f3d2fc85cfb",
-        ImageID: 27170,
-        ImageName: "iqe-test-image-8_5",
-        LastSeen: "2022-09-14T20:06:39.584623Z",
-        UpdateAvailable: true,
-        Status: "RUNNING",
-        ImageSetID: 8102,
-        DeviceGroups: [
-          {
-            ID: 52760,
-            Name: "test_device_groups_api_LmGwoYi7dz88OJ9Im",
-          },
-        ],
-        DispatcherStatus: "",
-        DispatcherReason: "",
-      },
-      {
-        DeviceID: 164011,
-        DeviceName: "maeve-test-device3",
-        DeviceUUID: "c921f9cf-8042-46a8-9b3c-0f3d2fc85cfb",
-        ImageID: 27170,
-        ImageName: "iqe-test-image-8_5",
-        LastSeen: "2022-09-14T20:06:39.584623Z",
-        UpdateAvailable: true,
-        Status: "RUNNING",
-        ImageSetID: 8102,
-        DeviceGroups: [
-          {
-            ID: 52760,
-            Name: "test_device_groups_api_LmGwoYi7dz88OJ9Im",
-          },
-        ],
-      },
-    ];
-
     //Click button while enabled
     cy.get(".pf-c-button.pf-m-primary")
       .contains("Update")
