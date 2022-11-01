66d716375acaa7b4310eac6be7e941a2b86aaf15
Tasos Papaioannou <tpapaioa@redhat.com>
2022-11-01 12:51:35 -0400

    Clean up custom repo and custom package steps in image wizard

--- src/components/form/WizardRepositoryTable.js
+++ src/components/form/WizardRepositoryTable.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import GeneralTable from '../general-table/GeneralTable';
 import { ExternalLinkAltIcon } from '@patternfly/react-icons';
 import { Text, TextVariants } from '@patternfly/react-core';
@@ -12,14 +12,12 @@ import useFieldApi from '@data-driven-forms/react-form-renderer/use-field-api';
 import { truncateString } from '../../utils';
 const filters = [{ label: 'Name', type: 'text' }];
 
-const WizardRepositoryTable = ({ ...props }) => {
+const WizardRepositoryTable = (props) => {
   const { change, getState } = useFormApi();
   const { input } = useFieldApi(props);
-  const wizardState = getState()?.values?.[input.name];
   const isUpdateWizard = getState()?.values?.isUpdate;
   const imageID = getState()?.values?.imageID;
 
-  const [selectedRepos, setSelectedRepos] = useState([]);
   const [response, fetchRepos] = useApi({
     api: ({ query }) =>
       getCustomRepositories({
@@ -30,17 +28,13 @@ const WizardRepositoryTable = ({ ...props }) => {
   });
   const { data, isLoading, hasError } = response;
 
-  useEffect(() => {
-    change(input.name, selectedRepos);
-  }, [selectedRepos]);
-
   const getRepoIds = (checked) => {
     const checkedRepos = checked?.map((repo) => ({
       id: repo?.id,
       name: repo?.name,
       URL: repo?.URL,
     }));
-    setSelectedRepos(checkedRepos);
+    input.onChange(checkedRepos);
   };
 
   useEffect(() => {
@@ -74,7 +68,7 @@ const WizardRepositoryTable = ({ ...props }) => {
 
   return (
     <>
-      {isLoading !== true && !data?.count > 0 ? (
+      {!isLoading && !data?.count > 0 ? (
         <EmptyState
           icon="repository"
           title="No custom repositories available"
@@ -101,7 +95,7 @@ const WizardRepositoryTable = ({ ...props }) => {
           defaultSort={{ index: 0, direction: 'desc' }}
           hasCheckbox={true}
           selectedItems={getRepoIds}
-          initSelectedItems={wizardState}
+          initSelectedItems={input.value}
         />
       )}
     </>
