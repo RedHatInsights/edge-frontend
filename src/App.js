72adb81cf39cfaaf269b6c0b8e148e1b180906a2
matoval <mat24c@gmail.com>
2022-12-28 12:57:22 -0800

    Add chrome init back in

--- src/App.js
+++ src/App.js
@@ -14,6 +14,8 @@ const App = (props) => {
   const history = useHistory();
   const [isAuth, setIsAuth] = useState(null);
   useEffect(() => {
+    insights.chrome.init();
+    // TODO change this to your appname
     insights.chrome.identifyApp('fleet-management');
 
     insights.chrome.on('APP_NAVIGATION', (event) =>
