6adcd50180067bb895ac83d45fc5d4da5722ce0a
acosferreia <acosferreira@gmail.com>
2023-03-22 14:22:18 -0700

    adjust e2e parameters

--- run_e2e.sh
+++ run_e2e.sh
@@ -4,5 +4,5 @@ mkdir $WK_DIR
 cp . -r $WK_DIR/
 cd $WK_DIR
 npm i
-npx cypress run --e2e --spec cypress/e2e/images/Images.cy.js
-npx cypress run --e2e --spec cypress/e2e/systems/Systems.cy.js
+npx cypress run --env configFile=e2e --e2e --spec cypress/e2e/images/Images.cy.js
+npx cypress run --env configFile=e2e --e2e --spec cypress/e2e/systems/Systems.cy.js
