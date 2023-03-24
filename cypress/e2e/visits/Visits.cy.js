54cfddae1c914499238d30d219dd0bef6eeaa793
acosferreia <acosferreira@gmail.com>
2023-03-24 16:24:45 -0700

    create simple visit tests

--- cypress/e2e/visits/Visits.cy.js
+++ cypress/e2e/visits/Visits.cy.js
@@ -1,13 +1,35 @@
-describe('Visit links', () => {
-  it('manage images happy path', () => {
-  
+describe('Visit Images', () => {
+  before(() => {
     cy.beforeTest('/manage-images');
+  });
+  it('manage images happy path', () => {
+    cy.url().should('include', '/manage-images')
+  });  
+});
 
+describe('Visit Repositories', () => {
+  before(() => {
     cy.beforeTest('/repositories');
- 
-    cy.beforeTest('/inventory');
-  
-    cy.beforeTest('/fleet-management');
+  });
+  it('manage repositories happy path', () => { 
+    cy.url().should('include', '/repositories')
+  });  
+});
 
+describe('Visit Inventory', () => {
+  before(() => {
+    cy.beforeTest('/inventory');
+  });
+  it('manage inventory happy path', () => {   
+    cy.url().should('include', '/inventory')
+  });  
+});
+
+describe('Visit Groups', () => {
+  before(() => {
+    cy.beforeTest('/fleet-management');
+  });
+  it('manage groups happy path', () => {
+    cy.url().should('include', '/fleet-management')
   });  
 });
