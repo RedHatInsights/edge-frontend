7fe296fc1c05174d78ec3bda26838a20ba2a7e83
Adelia Ferreira <adferrei@redhat.com>
2023-10-23 11:54:01 +0100

    Fix table align removing padding from button link
    Signed-off-by: Adelia Ferreira <adferrei@redhat.com>

--- src/Routes/Devices/DeviceTable.js
+++ src/Routes/Devices/DeviceTable.js
@@ -10,7 +10,6 @@ import CustomEmptyState from '../../components/Empty';
 import { createLink, emptyStateNoFilters, useFeatureFlags } from '../../utils';
 import DeviceStatus, { getDeviceStatus } from '../../components/Status';
 import RetryUpdatePopover from './RetryUpdatePopover';
-import { Button } from '@patternfly/react-core';
 import {
   FEATURE_HIDE_GROUP_ACTIONS,
   FEATURE_PARITY_INVENTORY_GROUPS,
@@ -178,16 +177,7 @@ const createRows = (
         {
           title: ImageName ? (
             hasLinks ? (
-              <Button
-                variant="link"
-                target-href={pathToImage}
-                onClick={(e) => {
-                  e.preventDefault();
-                  window.location.href = `${pathToImage}`;
-                }}
-              >
-                {ImageName}
-              </Button>
+              <a href={pathToImage}>{ImageName}</a>
             ) : (
               ImageName
             )
